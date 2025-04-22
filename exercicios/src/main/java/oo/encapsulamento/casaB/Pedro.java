dfecd2f081eafce47eb683a61b047494456d2271
Eduardo Faria <eduardofaria.rj@gmail.com>
2025-04-22 06:27:22 -0300

    Desafio Polimorfismo

--- exercicios/src/main/java/oo/encapsulamento/casaB/Pedro.java
+++ exercicios/src/main/java/oo/encapsulamento/casaB/Pedro.java
@@ -9,7 +9,7 @@ public class Pedro extends Ana {
     void testeAcessos (){
         /// System.out.println(mae.segredo);
         //System.out.println(mae.facoDentroDeCasa); // fora do pacote não é acessível, mesmo que por herança.
-        System.out.println(mae.formaDeFalar); // default, Fora do pacote, PROTECTED não acessa por herança uma instância da SUPER Classe MAS:
+        //System.out.println(mae.formaDeFalar); // default, Fora do pacote, PROTECTED não acessa por herança uma instância da SUPER Classe MAS:
         System.out.println(mae2.formaDeFalar); // Acessa por herança uma instância de si mesmo com o que foi herdado da SUPER Classe, não por composição.
         System.out.println(formaDeFalar); // OU pode acessar diretamente, sem necessidade de instanciar, pois é herdado da SUPER Classe.
         System.out.println(todosSabem); // public Acessado diretamente
