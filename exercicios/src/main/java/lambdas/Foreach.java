76d5a1893910896a1d01aa871202c27a4ec5603a
Eduardo Faria <eduardofaria.rj@gmail.com>
2025-04-25 22:21:34 -0300

    Programação funcional

---
+++ exercicios/src/main/java/lambdas/Foreach.java
@@ -0,0 +1,41 @@
+package lambdas;
+
+import colecoes.Lista;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Foreach {
+    public static void main(String[] args) {
+        List<String> aprovados = Arrays.asList("Ana", "Bia", "Lia", "Gui");
+
+        System.out.println("Forma tradicional...");
+        for (String nome : aprovados){
+            System.out.println(nome);
+        }
+
+        System.out.println("\nLambda #1...");
+        //sout
+        aprovados.forEach(n -> System.out.println(n + "!!!"));
+
+
+        /* IMPORTANTE */
+        System.out.println("\nMethod Reference #1...");
+        //soutc  //System.out::
+        aprovados.forEach(System.out::println);
+        // Significa: "Para cada item da List aprovados (.forEach()) chame .println() de System.out"
+
+
+        System.out.println("\nLambda #2...");
+        aprovados.forEach(n -> meuImprimir(n));
+
+
+        System.out.println("\nMethod Reference #2...");
+        aprovados.forEach(Foreach::meuImprimir);
+        // Usa o metodo estático (função) .meuImprimir() da classe Foreach() em cada item (.forEach(n)) da List aprovados.
+    }
+
+    static void meuImprimir(String nome) {
+        System.out.println("Oi! Meu nome é " + nome);
+    }
+}
